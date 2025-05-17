b91aee24ec73cd4e87e84edb79894286a33cea58
Sanjaya Poudel <poudelsanjaya12@gmail.com>
2025-05-17 14:05:05 +0545

    Add profile component and update app structure
    Introduced a new ProfileComponent with basic template and styles. Updated the app component to include login, signup, and profile elements in the HTML and imports. This enhances the app's functionality and layout.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {SignupComponent} from './signup/signup.component';
+import {ProfileComponent} from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [ LoginComponent, SignupComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector:"app-profile",
+  template:`<h1>Profile</h1>`,
+  styles:"h1{color: blue}"
+
+})
+export class ProfileComponent{
+
+}
